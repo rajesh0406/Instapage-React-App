a5415eb767ebb3a3caed4a6a75ea386e3a124d8a
Rajesh <rajgan8428800171@gmail.com>
2020-07-30 22:02:07 +0530

    Updates

---
+++ precache-manifest.78a420ddddd333911f22ad0238873dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a32040cc2ac5e4172c53a4ff6291f5b",
+    "url": "/Instapage-React-App/index.html"
+  },
+  {
+    "revision": "844d6e5719d86706da34",
+    "url": "/Instapage-React-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4a0a41a4665c8ce28c17",
+    "url": "/Instapage-React-App/static/css/main.ec385015.chunk.css"
+  },
+  {
+    "revision": "844d6e5719d86706da34",
+    "url": "/Instapage-React-App/static/js/2.a0550fb5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Instapage-React-App/static/js/2.a0550fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0a41a4665c8ce28c17",
+    "url": "/Instapage-React-App/static/js/main.ef0d8bd0.chunk.js"
+  },
+  {
+    "revision": "e799df668de952474039",
+    "url": "/Instapage-React-App/static/js/runtime-main.479e6ff0.js"
+  }
+]);
